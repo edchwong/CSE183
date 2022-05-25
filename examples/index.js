7ee99482e77b1f32cda040fe286798fda02f9922
edchwong <73096416+edchwong@users.noreply.github.com>
2022-05-25 16:29:13 -0700

    update stuff

---
+++ examples/index.js
@@ -0,0 +1,66 @@
+
+
+
+// This will be the object that will contain the Vue attributes
+// and be used to initialize it.
+let app = {};
+
+
+// Given an empty app object, initializes it filling its attributes,
+// creates a Vue instance, and then initializes the Vue instance.
+let init = (app) => {
+
+    // This is the Vue data.
+    app.data = {
+        // Complete as you see fit.
+    };
+
+    app.enumerate = (a) => {
+        // This adds an _idx field to each element of the array.
+        let k = 0;
+        a.map((e) => {e._idx = k++;});
+        return a;
+    };
+
+    app.set_chart = function () {
+        app.chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    }
+
+    app.draw = function () {
+        let data = ... 
+        let options = ... 
+        app.chart.draw(data, options);
+    }
+
+    // This contains all the methods.
+    app.methods = {
+        // Complete as you see fit.
+    };
+
+    // This creates the Vue instance.
+    app.vue = new Vue({
+        el: "#vue-target",
+        data: app.data,
+        methods: app.methods
+    });
+
+    // And this initializes it.
+    app.init = () => {
+        // Put here any initialization code.
+        // Typically this is a server GET call to load the data.
+    };
+
+    // Call to the initializer.
+    app.init();
+};
+
+// This takes the (empty) app object, and initializes it,
+// putting all the code i
+init(app);
+
+// Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(app.set_chart);
+
